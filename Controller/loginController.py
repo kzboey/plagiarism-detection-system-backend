9bb8fea24e30b42e2257df6eb6c6fb8b0b56a481
Mr. BOEY Kai Zhe <kaiboey2@ad.cityu.edu.hk>
2021-10-03 21:15:28 +0800

    03-10-2021

--- Controller/loginController.py
+++ Controller/loginController.py
@@ -1,8 +1,8 @@
-from flask import Flask, Blueprint, jsonify
+from flask import Blueprint, jsonify
 
 login = Blueprint('login', __name__)
 
 #entry point for login api
-@login.route('/login')
+@login.route('/login', methods = ['POST'])
 def index():
     return jsonify(status=0,message='success')
