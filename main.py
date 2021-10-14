7b65e63af14590404fa5b3c91238be82212c3fe0
Mr. BOEY Kai Zhe <kaiboey2@ad.cityu.edu.hk>
2021-10-14 21:26:54 +0800

    14-10-2021

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
-from pdf2image import convert_from_path
 import fitz
 
+def convertToPng(file):
+    try:
+        doc = fitz.open(file)
+        page_count = len(doc)
+        for idx in range(page_count):
+            page = doc.loadPage(idx)  # number of page
+            pix = page.getPixmap()
+            output = "outfile{}.png".format(idx)
+            pix.writePNG(output)
+    except:
+        print("File exception")
+
 def main():
-    pdffile = "Question 3_LIANG Qihang_55917602.pdf"
-    doc = fitz.open(pdffile)
-    page_count = len(doc)
-    for idx in range(page_count):
-        page = doc.loadPage(idx)  # number of page
-        pix = page.getPixmap()
-        output = "outfile{}.png".format(idx)
-        pix.writePNG(output)
+    myfile = "Question 3_LIANG Qihang_55917602.pdf"
+    convertToPng(myfile)
 
 if __name__ == "__main__":
     main()
+
